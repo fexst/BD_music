f82724f8b389d48ae2d7644ec56a1ffe458b67d0
fexst <160356079+fexst@users.noreply.github.com>
2025-01-20 21:06:02 +0300

    Update sql_select_2.sql

--- sql_select_2.sql
+++ sql_select_2.sql
@@ -10,10 +10,15 @@ select a.albums, avg(t.duration) from albumsinfo a
 join trackinfo t on a.albumsid = t.albumsid 
 group by a.albums;
 
-select musician from musician_albums ma 
-join musicianinfo m on m.musicianid = ma.musicianid 
-join albumsinfo a on ma.albumsid = a.albumsid 
-where a."year" not between '2020-01-01' and '2020-12-31';
+select musician 
+from musicianinfo
+where musician not in (
+select musician 
+from musicianinfo m
+join musician_albums ma on m.musicianid = ma.musicianid
+join albumsinfo a on a.albumsid = ma.albumsid
+where a."year" between '2020-01-01' and '2020-12-31'
+);
 
 select collection from track_collection tc 
 join collectioninfo c on c.collectionid = tc.collectionid
